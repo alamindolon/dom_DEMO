4cd885313e23b4999b79164809d165019e80fc40
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-02-21 10:36:21 +0600

    of style use

---
+++ jss/dom.js
@@ -0,0 +1,10 @@
+const sections  = document.querySelectorAll('section')
+for(const section of sections)
+{
+    //console.log(section)
+
+    section.style.border = '2px solid steelblue';
+    section.style.borderRadius = '15PX'
+    section.style.marginBottom = '10px'
+    section.style.padding = '7px'
+}
