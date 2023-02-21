fd6b9fd297824f535db6092ca483fbb31e0086bf
Al Amin Sarker Dolon <80084331+alamindolon@users.noreply.github.com>
2023-02-21 13:47:18 +0600

    set text & set innerhtml

---
+++ jss/append.js
@@ -0,0 +1,49 @@
+const mainContainer = document.getElementById('main_container');
+
+const section = document.createElement('section');
+const h1 = document.createElement('h1');
+h1.innerText = 'MY Food list';
+section.appendChild(h1);
+
+const ul = document.createElement('ul');
+
+const li1 = document.createElement('li');
+li1.innerText = 'vat';
+ul.appendChild(li1);
+
+const li2 = document.createElement('li');
+li2.innerText = 'mas';
+ul.appendChild(li2);
+
+const li3 = document.createElement('li');
+li3.innerText = 'sobji';
+ul.appendChild(li3);
+
+const li4 = document.createElement('li');
+li4.innerText = 'votta';
+ul.appendChild(li4);
+
+section.appendChild(ul);
+mainContainer.appendChild(section)
+
+//set innnerHTML directly
+
+const sectionDress  = document.createElement('section');
+
+sectionDress.innerHTML = `
+<h1>my Dress section</h1>
+
+<ul>
+    <li> T-shirt</li>
+    <li> polo-shirt</li>
+    <li> sando ganji</li>
+    
+</ul>
+
+
+
+`
+mainContainer.appendChild(sectionDress);
+
+
+
